3087da9e4064efbb970b68d30ebeb76f05e22634
Martin Forstner <martin@redislabs.com>
2019-08-07 20:05:56 +0200

    implemnt a Redis failover mechanism

---
+++ src/main/java/com/redislabs/demo/failover/Failover.java
@@ -0,0 +1,97 @@
+package com.redislabs.demo.failover;
+
+import io.lettuce.core.RedisCommandTimeoutException;
+import io.lettuce.core.RedisConnectionException;
+import io.lettuce.core.RedisException;
+
+import java.util.logging.Logger;
+
+public class Failover {
+
+    private static Logger logger = Logger.getLogger(Failover.class.getName());
+
+    private RedisConnection[] connections;
+
+    private int currentConnectionIdx = 0;
+
+    public static void main(String[] args) {
+
+        Failover fo = new Failover();
+
+        fo.createConnections(args);
+
+        fo.configureReplication();
+
+        // clear database
+        fo.connections[fo.currentConnectionIdx].exec().flushall();
+
+        fo.runComputation();
+
+        for (RedisConnection conn : fo.connections) {
+            logger.info("shutting down " + conn.getUrl());
+            conn.close();
+            conn.shutdown();
+        }
+
+    }
+
+    public void createConnections(String[] urls) {
+        this.connections = new RedisConnection[urls.length];
+        for (int i = 0; i < urls.length; i++) {
+            String[] parts  = urls[i].split(":", 4);
+            this.connections[i] = new RedisConnection(
+                    parts[0],
+                    Integer.parseInt(parts[1]),
+                    parts[2],
+                    Integer.parseInt(parts[3]),
+                    10);
+        }
+    }
+
+    public void configureReplication() {
+        for (int i = 0; i < this.connections.length; i++) {
+            if (i == currentConnectionIdx) {
+                try {
+                    this.connections[i].exec().slaveofNoOne();
+                } catch (RedisException e) {
+                    logger.severe(e.toString());
+                    this.performFailover();
+                    break;
+                }
+            } else {
+                try {
+                    this.connections[i].exec().slaveof(
+                            this.connections[this.currentConnectionIdx].getInternalHost(),
+                            this.connections[this.currentConnectionIdx].getInternalPort()
+                    );
+                } catch (RedisException e) {
+                    logger.severe(e.toString());
+                    continue;
+                }
+            }
+        }
+    }
+
+    public void runComputation() {
+        logger.info("running against connection " + this.connections[this.currentConnectionIdx].getUrl());
+        try {
+            this.connections[this.currentConnectionIdx].putTraffic();
+        } catch (RedisCommandTimeoutException e) {
+            logger.severe(e.toString());
+            try {
+                this.connections[this.currentConnectionIdx].exec().ping();
+            } catch (RedisConnectionException e2) {
+                logger.severe(e2.toString());
+                this.performFailover();
+            }
+            this.runComputation();
+        }
+    }
+
+    public void performFailover() {
+        this.currentConnectionIdx = (this.currentConnectionIdx + 1) % this.connections.length;
+        logger.warning("failing over to connection " + this.connections[this.currentConnectionIdx].getUrl());
+        this.configureReplication();
+    }
+
+}
---
+++ src/main/java/com/redislabs/demo/failover/RedisConnection.java
@@ -0,0 +1,93 @@
+package com.redislabs.demo.failover;
+
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+
+import java.nio.charset.Charset;
+import java.util.Random;
+
+public class RedisConnection {
+
+    private String internalHost;
+
+    private int internalPort;
+
+    private String externalHost;
+
+    private int externalPort;
+
+    private String url;
+
+    private RedisClient client;
+
+    private StatefulRedisConnection<String, String> connection;
+
+    private RedisCommands<String, String> syncCommands;
+
+    public RedisConnection(String intHost, int intPort, String extHost, int extPort, int timeout) {
+        this.internalHost = intHost;
+        this.internalPort = intPort;
+        this.externalHost = extHost;
+        this.externalPort = extPort;
+        this.url = "redis://" + externalHost + ':' + externalPort + "?timeout=" + timeout + 's';
+        this.client = RedisClient.create(this.url);
+        this.client.setOptions(ClientOptions.builder()
+            .autoReconnect(true)
+            .build());
+        this.connect();
+    }
+
+    public String getInternalHost() { return this.internalHost; }
+
+    public int getInternalPort() { return this.internalPort; }
+
+    public String getExternalHost() { return this.externalHost; }
+
+    public int getExternalPort() { return this.externalPort; }
+
+    public String getUrl() { return this.url; }
+
+    public RedisCommands<String, String> exec() {
+        if (!this.connection.isOpen()) {
+            this.connect();
+        }
+        return this.syncCommands;
+    }
+
+    public void putTraffic() {
+        if (!this.connection.isOpen()) {
+            this.connect();
+        }
+        while(true) {
+            String key = getRandomString(12);
+            syncCommands.set(key, getRandomString(32));
+            syncCommands.get(key);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+
+    public void close() {
+        connection.close();
+    }
+
+    public void shutdown() {
+        client.shutdown();
+    }
+
+    private void connect() {
+        this.connection = this.client.connect();
+        this.syncCommands = this.connection.sync();
+    }
+
+    private String getRandomString(int length) {
+        byte[] array = new byte[length];
+        new Random().nextBytes(array);
+        return new String(array, Charset.forName("UTF-8"));
+    }
+}
